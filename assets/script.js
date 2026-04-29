1f459d74f8850a4a944d7a1aba5c35b4dc382237
Kyle T Territo <kterri3@lsu.edu>
2026-04-28 19:00:55 -0500

    bulk update

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,15 @@ if (toggle && links) {
     });
   });
 }
+
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+if (slides.length > 1) {
+  setInterval(() => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+  }, 4000);
+}
